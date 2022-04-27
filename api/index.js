eeaebcd7a4491fd472a08802bfb606662528897c
raidenblackout <xcdtlatin2@gmail.com>
2022-04-27 20:31:21 +0600

    test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client({intents: 'GUILD_MESSAGES'}); //create new client
+const client = new Discord.Client({intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]}); //create new client
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
+client.on('messageCreate', msg => {
+  console.log('message');
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN);
