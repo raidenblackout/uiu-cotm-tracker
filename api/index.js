034720447e3c230df2f20622cb04ebd6752cde8c
raidenblackout <xcdtlatin2@gmail.com>
2022-04-27 22:43:53 +0600

    test

--- api/index.js
+++ api/index.js
@@ -1,32 +1,105 @@
 const {
   InteractionResponseType,
   InteractionType,
-  verifyKeyMiddleware,
-} = require("discord-interactions");
-const express = require("express");
-const { get } = require("http");
-const app = express();
+  verifyKey,
+} = require('discord-interactions');
+const getRawBody = require('raw-body');
 
-// app.get('/',(req,res)=>{
-//   res.setHeader('Content-Type', 'text/html');
-//   // res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-//   res.send("Hello World!");
-// });
-app.post('/api', verifyKeyMiddleware('4fb933cfe1d3038b611d700d6f7a12f3843f03ba20bded60da82f37cbcb07d4b'), (req, res) => {
-  const message = req.body;
-  if (message.type === InteractionType.APPLICATION_COMMAND) {
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: 'Hello world',
-      },
-    });
-  }else if (message.type === InteractionType.PING) {
-    console.log("Handling Ping request");
-    response.send({
-      type: InteractionResponseType.PONG,
-    });
+const SLAP_COMMAND = {
+  name: 'Slap',
+  description: 'Sometimes you gotta slap a person with a large trout',
+  options: [
+    {
+      name: 'user',
+      description: 'The user to slap',
+      type: 6,
+      required: true,
+    },
+  ],
+};
+
+const INVITE_COMMAND = {
+  name: 'Invite',
+  description: 'Get an invite link to add the bot to your server',
+};
+
+const SUPPORT_COMMAND = {
+  name: 'Support',
+  description: 'Like this bot? Support me!',
+};
+
+const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${process.env.APPLICATION_ID}&scope=applications.commands`;
+
+/**
+ * Gotta see someone 'bout a trout
+ * @param {VercelRequest} request
+ * @param {VercelResponse} response
+ */
+module.exports = async (request, response) => {
+  if (request.method === 'POST') {
+    const signature = request.headers['x-signature-ed25519'];
+    const timestamp = request.headers['x-signature-timestamp'];
+    const rawBody = await getRawBody(request);
+
+    const isValidRequest = verifyKey(
+      rawBody,
+      signature,
+      timestamp,
+      process.env.PUBLIC_KEY
+    );
+
+    if (!isValidRequest) {
+      console.error('Invalid Request');
+      return response.status(401).send({ error: 'Bad request signature ' });
+    }
+
+    const message = request.body;
+
+    if (message.type === InteractionType.PING) {
+      console.log('Handling Ping request');
+      response.send({
+        type: InteractionResponseType.PONG,
+      });
+    } else if (message.type === InteractionType.APPLICATION_COMMAND) {
+      switch (message.data.name.toLowerCase()) {
+        case SLAP_COMMAND.name.toLowerCase():
+          response.status(200).send({
+            type: 4,
+            data: {
+              content: `*<@${message.member.user.id}> slaps <@${message.data.options[0].value}> around a bit with a large trout*`,
+            },
+          });
+          console.log('Slap Request');
+          break;
+        case INVITE_COMMAND.name.toLowerCase():
+          response.status(200).send({
+            type: 4,
+            data: {
+              content: INVITE_URL,
+              flags: 64,
+            },
+          });
+          console.log('Invite request');
+          break;
+        case SUPPORT_COMMAND.name.toLowerCase():
+          response.status(200).send({
+            type: 4,
+            data: {
+              content:
+                "Thanks for using my bot! Let me know what you think on twitter (@IanMitchel1). If you'd like to contribute to hosting costs, you can donate at https://github.com/sponsors/ianmitchell",
+              flags: 64,
+            },
+          });
+          console.log('Support request');
+          break;
+        default:
+          console.error('Unknown Command');
+          response.status(400).send({ error: 'Unknown Type' });
+          break;
+      }
+    } else {
+      console.error('Unknown Type');
+      response.status(400).send({ error: 'Unknown Type' });
+    }
   }
-});
-
-module.exports = app;
+};
